06b0413124ebaea448cb387e3aac18a5edf7c53c
shddd <61633436+shddd@users.noreply.github.com>
2021-08-03 11:21:39 +0200

    Update introsort.py

--- introsort.py
+++ introsort.py
@@ -6,19 +6,43 @@
 import random
 import math
 
+def heap(A, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and A[i] < A[l]:
+        largest = l
+    if r < n and A[largest] < A[r]:
+        largest = r
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        heap(A, n, largest)
+  
+  
 def heapsort(A):
+    n = len(A)
+    for i in range(n//2, -1, -1):
+        heap(A, n, i)
+    for i in range(n-1, 0, -1):
+        A[i], A[0] = A[0], A[i]
+        heap(A, i, 0)
+    return A
+  
+#Just for testing
+A = [1, 25436, 12, 24, 6, 2]
+print(heapsort(A))
 	
 
 
 
 def sort(A):
-    maxdepth = floor(log(len(A)) * 2)
+    maxdepth = math.floor(math.log(len(A)) * 2)
     return introsort(A, maxdepth)
 
 def introsort(A, maxdepth):
-	n = len(A)
-	if n <= 1:
-	    return A
+    n = len(A)
+    if n <= 1:
+        return A
     elif (maxdepth == 0):
         heapsort(A)
     else :
@@ -29,4 +53,4 @@ def introsort(A, maxdepth):
 
 #just for testing
 a = [6, 4, 2, 1, 3, 5]
-print(introsort(a))
+print(sort(a))
