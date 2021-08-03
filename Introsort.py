7f68ac90e6d1663d004c05020976e7576fd4ac45
shddd <61633436+shddd@users.noreply.github.com>
2021-08-03 10:13:33 +0200

    Create Introsort.py

---
+++ Introsort.py
@@ -0,0 +1,15 @@
+#Introsort
+#Complexité en temps:En moyenne O(n log n) Au pire O(n log n)
+#Entrée : une liste A
+#Sortie : A classée dans l'ordre croissant
+
+import random
+
+def introsort(A):
+	n = len(A)
+	
+
+
+#just for testing
+a = [6, 4, 2, 1, 3, 5]
+print(introsort(a))
