4dddd5873275ea168cb7b1d541181d29154d19b5
shddd <61633436+shddd@users.noreply.github.com>
2021-08-04 14:26:51 +0200

    Update tri de Shell.py

--- tri de Shell.py
+++ tri de Shell.py
@@ -5,9 +5,19 @@
 #Sortie : A classée dans l'ordre croissant
 
 def Shellsort(A):
-	
-
-
+    n = len(A)
+    j = 0
+    temp = 0
+    E = [701, 301, 132, 57, 23, 10, 4, 1]
+    for e in E:
+        for i in range (e, n):
+            temp = A[i]
+            j = i
+            while ((j >= e) and (A[j-e] > temp)):
+                A[j] = A[j-e]
+                j = j-e
+                A[j] = temp
+    return A
 
 #Just for testing
 A = [125, 142, 12, 11, 24, 2, 1]
