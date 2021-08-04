946e59c5a21cef249b2148209f9421e65f609556
shddd <61633436+shddd@users.noreply.github.com>
2021-08-04 12:10:43 +0200

    Create tri de Shell.py

---
+++ tri de Shell.py
@@ -0,0 +1,14 @@
+#Tri de Shell / Shell sort
+#Complexité en temps : Au mieux O(n log² n), Au pire O(n²)
+#Complexité en espace : Au pire O(1)
+#Entrée : une liste A
+#Sortie : A classée dans l'ordre croissant
+
+def Shellsort(A):
+	
+
+
+
+#Just for testing
+A = [125, 142, 12, 11, 24, 2, 1]
+print(Shellsort(A))
