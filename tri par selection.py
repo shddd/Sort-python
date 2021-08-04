9fb40ad5ecb78c45b9f30981392767d925abebf2
shddd <61633436+shddd@users.noreply.github.com>
2021-08-04 14:28:49 +0200

    Create tri par selection.py

---
+++ tri par selection.py
@@ -0,0 +1,11 @@
+#Tri de Shell / Selection sort
+#Complexité en temps : Au mieux O(n²), En moyenne O(n²), Au pire O(n²)
+#Complexité en espace : Au pire O(1)
+#Entrée : une liste A
+#Sortie : A classée dans l'ordre croissant
+
+def selsort(A):
+
+#Just for testing
+A = [125, 142, 12, 11, 24, 2, 1]
+print(selsort(A))
